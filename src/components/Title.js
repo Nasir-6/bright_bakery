2a26b1767bae88ca574870c9e083805a4f5aa772
Nasir <nasir-islam@hotmail.com>
2022-03-15 23:49:41 +0000

    add open/close button and functionality using useState

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import {useState} from 'react';
 
 const Title = () => {
     
-    const status = "open";
+    const [status, setStatus] = useState("open");
+
+    const handleClick  = () => {
+        status === "open" ? setStatus("closed") : setStatus("open");
+    }
     return (
         <>
             <h1>The Bright Bakery</h1>
             <p>est 2022</p>
-            <h4 className='red'>{status}</h4>
+            <h4 className='red'>The bakery is now {status}!</h4>
+            <button onClick={handleClick}>Open/Closed</button>
         </>
     )
 
