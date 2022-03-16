62eff98313c1ef4ff331fe2cee21d3dd968ed694
Nasir <nasir-islam@hotmail.com>
2022-03-16 01:22:58 +0000

    add inputs and state handler and also submit handler

--- src/components/RecipeForm.js
+++ src/components/RecipeForm.js
@@ -1,12 +1,54 @@
+import {useState} from 'react'
 
 const RecipeForm = () => {
+
+    // Set all states to empty at start
+    const [cakeName, setCakeName] = useState("");
+    const [ingredients, setIngredients] = useState("");
+    const [rating, setRating] = useState("");
+
+    // create onchange event handlers!!
+    const handleCakeNameChange = (event) => {
+        setCakeName(event.target.value);
+    }
+
+    const handleIngredientsChange = (event) => {
+        setIngredients(event.target.value);
+    }
+
+    const handleRatingChange = (event) => {
+        setRating(event.target.value);
+    }
+
+    // Form submit handler
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+        //validate inputs (if empty just exits don't submit or do anything)
+        if(!cakeName || !ingredients || !rating){
+            return
+        }
+
+
+
+        // Lastly reset all input states
+        setCakeName("")
+        setIngredients("")
+        setRating("")
+    }
+
     return(
         <>
-            <form>
-                <label for="fname">Cake name:</label>
-                <input type="text" id="cakename" name="cakename" />
-                <label for="ingredients">Ingredients:</label>
-                <input type="text" id="ingredients" name="ingredients" />
+            <form onSubmit={handleFormSubmit}>
+                <label htmlFor="fname">Cake name:</label>
+                <input type="text" id="cakename" name="cakename" value={cakeName} onChange={handleCakeNameChange} />
+
+                <label htmlFor="ingredients">Ingredients:</label>
+                <input type="text" id="ingredients" name="ingredients" value={ingredients} onChange={handleIngredientsChange} />
+
+                <label htmlFor="rating">Rating:</label>
+                <input type="text" id="rating" name="rating" value={rating} onChange={handleRatingChange}/>
+
+                <input type="submit" value="Post" />
             </form>
         </>
     )
