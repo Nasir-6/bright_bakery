adb7839fb4bbab036ea2422c8008901b1020e908
Nasir <nasir-islam@hotmail.com>
2022-03-16 01:26:00 +0000

    moved cakes state to app.js and passed down to recipeContainer as a prop

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import {useState} from 'react'
 import Title from './components/Title';
 import Search from './components/Search';
 import NavBar from './components/NavBar';
@@ -6,13 +7,57 @@ import RecipeForm from './components/RecipeForm';
 import RecipeContainer from './containers/RecipeContainer';
 
 function App() {
+
+  const cakes = [
+    {
+      cakeName: "Lemon Drizzle",
+      ingredients: [
+        "eggs",
+        "butter",
+        "lemon  zest",
+        "sugar",
+        "self-raising flour",
+      ],
+      rating: 5,
+    },
+    {
+      cakeName: "Tea Loaf",
+      ingredients: [
+        "eggs",
+        "oil",
+        "dried fruit",
+        "sugar",
+        "self-raising flour",
+        "strong tea",
+      ],
+      rating: 3,
+    },
+    {
+      cakeName: "Brownie",
+      ingredients: ["chocolate", "eggs", "flour", "butter", "walnuts"],
+      rating: 4,
+    },
+    {
+      cakeName: "Carrot Cake",
+      ingredients: [
+        "carrots",
+        "walnuts",
+        "oil",
+        "cream cheese",
+        "flour",
+        "sugar",
+      ],
+      rating: 5,
+    },
+  ];
+
   return (
     <>
       <Title />
       <Search />
       <NavBar />
       <RecipeForm />
-      <RecipeContainer />
+      <RecipeContainer cakes={cakes} />
     </>
   );
 }
